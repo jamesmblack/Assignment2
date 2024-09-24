1b8fc9284495dfc44544026e747454c90f1ada77
Michael Black <jb747216@sju.edu>
2024-09-24 18:27:39 -0400

    Create Restaurant queries.sql

---
+++ Restaurant queries.sql
@@ -0,0 +1,20 @@
+select restaurants.name, AVG(foods.price) from restaurants
+JOIN serves ON restaurants.restID = serves.restID
+JOIN foods ON serves.foodID = foods.foodID
+GROUP BY restaurants.name;
+
+select restaurants.name, max(foods.price) from restaurants
+JOIN serves ON restaurants.restID =serves.restID
+JOIN foods ON serves.foodID = foods.foodID
+GROUP BY restaurants.name;
+
+select restaurants.name, count(DISTINCT (foods.type)) from restaurants
+JOIN serves ON restaurants.restID = serves.restID
+JOIN foods ON serves.foodID = foods.foodID
+GROUP BY restaurants.name;
+
+select chefs.name, AVG(foods.price) from chefs
+JOIN chefs ON works.chefID = chefs.chefID
+JOIN serves ON works.restID = serves.restID
+JOIN foods ON serves.foodID = foods.foodID
+GROUP BY chefs.name;
