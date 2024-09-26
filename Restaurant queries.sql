747d5a450d68faeaa54b8b450f004f40c631203a
Michael Black <jb747216@sju.edu>
2024-09-26 11:41:07 -0400

    Update Restaurant queries.sql

--- Restaurant queries.sql
+++ Restaurant queries.sql
@@ -13,15 +13,25 @@ JOIN serves ON restaurants.restID = serves.restID
 JOIN foods ON serves.foodID = foods.foodID
 GROUP BY restaurants.name;
 
-select chefs.name, AVG(foods.price) from chefs
-JOIN chefs ON works.chefID = chefs.chefID
+SELECT chefs.name, AVG(foods.price) AS avg_price
+FROM chefs
+JOIN works ON chefs.chefID = works.chefID
 JOIN serves ON works.restID = serves.restID
 JOIN foods ON serves.foodID = foods.foodID
 GROUP BY chefs.name;
 
-SELECT restaurants.name, AVG(foods.price)
+SELECT restaurants.name, AVG(foods.price) AS avg_price
 FROM restaurants
 JOIN serves ON restaurants.restID = serves.restID
 JOIN foods ON serves.foodID = foods.foodID
 GROUP BY restaurants.name
 ORDER BY AVG(foods.price) DESC;
+
+SELECT chefs.name, AVG(foods.price) AS avg_food, GROUP_CONCAT( DISTINCT restaurants.name SEPARATOR ", ") AS restaurants
+FROM chefs
+JOIN works ON chefs.chefID = works.chefID
+JOIN restaurants ON works.restID = restaurants.restID
+JOIN serves ON restaurants.restID = serves.restID
+JOIN foods ON serves.foodID = foods.foodID
+GROUP BY chefs.name
+ORDER BY avg_food DESC;
